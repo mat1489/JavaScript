41d0ac16c03fdba5493d8c1543fa9383b34297b4
mat1489 <mat1489@op.pl>
2017-02-04 10:46:04 +0100

    4-tablice gotowe

---
+++ 4- tablice/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var imiona = new Array();
+var imionaStudentow= new Array(9);
+var imionaChlopakow = new Array('mateusz', 'michal','krzysiek', 'sergio');
+
+imionaStudentow[2]= "przemek";
+
+console.log(imiona);
+console.log(imionaStudentow);
+console.log(imionaChlopakow);
+
+
+var wiek = [];
+var wiekStudentow= [20,25,28];
+console.log(wiek);
+console.log(wiekStudentow);
