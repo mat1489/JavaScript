6da3447527af773fa194d7637b4d464a5ecda816
mat1489 <mat1489@op.pl>
2017-02-04 13:10:16 +0100

    9- operators operatory  gotowe

---
+++ 9- operators operatory/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var liczba1=13, liczba2=5;
+
+var wynik;
+wynik= liczba1 % liczba2;
+console.log(wynik);
+
+
+
+liczba1 -=3;
+console.log(liczba1);
+
+
+(2>5) ? consolelog("tak") : console.log("nie"); /* skrocony zapis petli if*/
+
+
+
