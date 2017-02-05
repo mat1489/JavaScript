b1dc6a978bffee71c3c769a9d5ccc97240cd619a
mat1489 <mat1489@op.pl>
2017-02-05 10:20:28 +0100

    13- objects gotowe2

--- 13-objects obiekty/js/script.js
+++ 13-objects obiekty/js/script.js
@@ -71,37 +71,63 @@ function Samochod(marka){     //nazwa funkcji zawsze z duzej litery
                     );
     }
 }
+console.log("----------------- obiekty w tablicy");
+
+var samochody=new Array();                      //nowa tablica
+
+samochody[0]=new Samochod('Peugeot');           //przypisanie do tablicy o indexie 0 nowego obiektu
+samochody[0].pojemnosc=1600;
+samochody[0].moc="110KM";
 
 
+samochody[1]=new Samochod('Audi');
+samochody[1].pojemnosc=1900;
+samochody[1].moc="150KM";
 
 
-var peugeot =new Samochod('Peugeot');
+samochody[2]=new Samochod('Bmw');
+samochody[2].pojemnosc=2500;
+samochody[2].moc="200KM";
+
+
+samochody[3]=new Samochod('Opel');
+samochody[3].pojemnosc=1600;
+samochody[3].moc="80KM";
+
+samochody.forEach(function(samochod, i){
+    console.log("samochod nr: "+ i);
+    samochod.wyswietlInfo();
+});
+
+
+console.log("----------------- obiekty");
+/*var peugeot =new Samochod('Peugeot');
 
 peugeot.pojemnosc=1600;
 peugeot.moc="110KM";
-
 peugeot.wyswietlInfo();
+
 
 
 var audi =new Samochod('Audi');
 
 audi.pojemnosc=1900;
 audi.moc="150KM";
-
 audi.wyswietlInfo();
+
+
 
 var bmw =new Samochod('Bmw');
 
 bmw.pojemnosc=2500;
 bmw.moc="200KM";
-
 bmw.wyswietlInfo();
+
 
 
 var opel =new Samochod('Opel');
 
 opel.pojemnosc=1600;
 opel.moc="80KM";
-
-opel.wyswietlInfo();
+opel.wyswietlInfo();*/
 
