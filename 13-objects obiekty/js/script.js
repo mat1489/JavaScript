deff16bfa742203ae160a35c7ae1d053024fa473
mat1489 <mat1489@op.pl>
2017-02-05 09:50:22 +0100

    13-objects obiekt klasa samochod

--- 13-objects obiekty/js/script.js
+++ 13-objects obiekty/js/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-
-var kaja = {                                           /* instancja klasy obiekt*/
+ /* instancja klasy obiekt*/
+/*
+var kaja = {                                          
     imie: "Kaja",
     wzrost: 152,
     przedstawOsobe: function(){console.log(this.imie)}
@@ -35,14 +36,72 @@ function Osoba(imie, nazwisko){     //nazwa funkcji zawsze z duzej litery
                     +"Oczy: "+this.oczy);
     }
 }
+*/
 
 
 
 /*tworzenie nowej instancji */
 
+/*
 var krystian =new Osoba('krystian', 'dziopa');
 
 krystian.wzrost=180;
 krystian.oczy="niebieskie";
 
 krystian.wyswietlInfo();
+
+*/
+
+
+
+
+
+//klasa samochod marka pojemnosc i moc. metode przedstawiajaca ten samochod
+
+
+function Samochod(marka){     //nazwa funkcji zawsze z duzej litery
+    this.marka=marka;
+   
+    this.pojemnosc;
+    this.moc;
+    this.wyswietlInfo=function(){
+        console.log("Marka: "+ this.marka + "\n"
+                   +"Pojemność: "+this.pojemnosc +"\n"
+                    +"Moc: "+this.moc
+                    );
+    }
+}
+
+
+
+
+var peugeot =new Samochod('Peugeot');
+
+peugeot.pojemnosc=1600;
+peugeot.moc="110KM";
+
+peugeot.wyswietlInfo();
+
+
+var audi =new Samochod('Audi');
+
+audi.pojemnosc=1900;
+audi.moc="150KM";
+
+audi.wyswietlInfo();
+
+var bmw =new Samochod('Bmw');
+
+bmw.pojemnosc=2500;
+bmw.moc="200KM";
+
+bmw.wyswietlInfo();
+
+
+var opel =new Samochod('Opel');
+
+opel.pojemnosc=1600;
+opel.moc="80KM";
+
+opel.wyswietlInfo();
+
